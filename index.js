4edc2d99e7cceddadbdde9bcaca27b4b162299aa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-14 07:02:06 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ const NameController = process.argv[2];
 
 //  check are there any controller name enterd
 if (!controllerName) {
-    console.error('Please provide a controller name');
+    console.error('Enter Controller name');
     process.exit(1);
 }
