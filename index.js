f3911a071922da339147594c5b6da99be982a5d0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-14 07:01:45 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const path = require('path');
 // now get controller name from terminal
 
 const NameController = process.argv[2];
+
+
+//  check are there any controller name enterd
+if (!controllerName) {
+    console.error('Please provide a controller name');
+    process.exit(1);
+}
