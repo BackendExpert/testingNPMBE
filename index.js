98930a30cc857dfbc7e55814d4ede0236688eb7f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-26 19:10:06 +0530

    Update index.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ async function createController(name) {
   await fs.writeFile(controllerPath, controllerContent.trim());
   console.log(`Controller ${name}Controller created at ${controllerPath}`);
 }
+
+async function createModel(name) {
+  const contentModel = `
+  const mongoose = require('mongoose')
+
+  const ${name}Schema = new mongoose.Schema({
+    // content of model
+  })
+  
+  const ${name} = mongoose.model('${name}', ${name}Schema)
+  
+  module.exports = ${name};
+  `
+
+  const modelPath = path.join('server', 'Models', `${name}.js`);
+
+  await fs.mkdir(path.dirname(modelPath), { recursive: true });
+
+  await fs.writeFile(modelPath, controllerContent.trim());
+  console.log(`Controller ${name}Controller created at ${modelPath}`);
+}
